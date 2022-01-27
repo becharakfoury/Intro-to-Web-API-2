0a0fb1bd80c6d2b29632103efd40d5f70317b8e1
becharakfoury <81804866+becharakfoury@users.noreply.github.com>
2022-01-27 20:56:47 +0200

    Add files via upload

---
+++ Models/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoAPI.Models
+{
+    /// <summary>
+    /// Represent One specific person
+    /// </summary>
+    public class Person
+    {
+
+        /// <summary>
+        ///  ID from SQL   
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        ///  users first name
+        /// </summary>
+        public string  FirstName { get; set; }
+
+        /// <summary>
+        ///  user last name
+        /// </summary>
+        public string  LastName { get; set; }        
+
+    }
+}
